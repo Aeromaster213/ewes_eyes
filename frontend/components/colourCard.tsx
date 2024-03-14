83a78ee2a412b316585d2ec1da45660a5eaa58a0
Abhinav <abhi.gupta8802@gmail.com>
2024-03-14 22:09:07 +0530

    color card

---
+++ frontend/components/colourCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ColourCardProps {
+    colours: { r: number, g: number, b: number }[];
+}
+
+export default function ColourCard({ colours }: ColourCardProps) {
+    return (
+        <div style={{ width: "100px", height: "70px" }} className="grid grid-cols-4 hover:cursor-pointer">
+            <div style={{ backgroundColor: `rgb(${colours[0].r}, ${colours[0].g}, ${colours[0].b})` }} className="col-start-1 col-end-2"></div>
+            <div style={{ backgroundColor: `rgb(${colours[1].r}, ${colours[1].g}, ${colours[1].b})` }} className="col-start-2 col-end-3"></div>
+            <div style={{ backgroundColor: `rgb(${colours[2].r}, ${colours[2].g}, ${colours[2].b})` }} className="col-start-3 col-end-4"></div>
+            <div style={{ backgroundColor: `rgb(${colours[3].r}, ${colours[3].g}, ${colours[3].b})` }} className="col-start-4 col-end-5"></div>
+        </div>
+    );
+}
--- frontend/components/selectcolour.tsx
+++ frontend/components/selectcolour.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { getInputColors } from "./functions";
+import ColourCard from "./colourCard";
 
 export default function SelectColour() {
     const colors = getInputColors();
     console.log(colors);
     return (
         <div>
-
+            <ColourCard colours={[{r: 125, g:125, b:125}, {r: 200, g: 200, b : 200}, {r:12, g: 45, b:34},{r:125,g:125,b:125}]} />
         </div>
     )
 }
