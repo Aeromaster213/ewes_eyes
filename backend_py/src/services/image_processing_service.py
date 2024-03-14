609b4270a065396adf22414b32eeed920cc9c12c
artemisia <arvind.srinivasn@gmail.com>
2024-03-14 22:31:40 +0530

    Gitignore and middleware changes

--- backend_py/src/services/image_processing_service.py
+++ backend_py/src/services/image_processing_service.py
@@ -3,7 +3,9 @@
 import os
 import shutil
 import time
+from src.utils import lib, image_utils
 from PIL import Image
+from src.main import get_loaded_model
 
 async def resize_image(image_path, output_file_path, width, height):
     try:
@@ -18,11 +20,12 @@ async def resize_image(image_path, output_file_path, width, height):
 
 async def generate_color_palette(image_path):
     # hello
-    return
+    image = Image.open(image_path)
+    return lib.kmeans(image, 4)
 
 async def generate_image(image_path, text_prompt):
     # hello
-    return
+    return image_utils.predict(get_load_model(), image_path, text_prompt)
 
 
 
